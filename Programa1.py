bd10332a8846026a8397dd7eeebbf79274ba7600
Maky188 <maky188pgt555@gmail.com>
2023-11-19 16:33:24 -0100

    Acerto

--- Programa1.py
+++ Programa1.py
@@ -1,7 +1,10 @@
 def pertence(lista, elemento):
-    for i in range(len(lista)):
+    i = 0
+    while i != len(lista):
         if lista[i] == elemento:
             return True
+        else:
+            i += 1
     return False
 
 print(pertence(lista = [2, 3, 4], elemento = int(input("elemento: "))))
